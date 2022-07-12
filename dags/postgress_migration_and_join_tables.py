ec5b67379054c93743880ab239f7e56bea572a1e
ayslanleal <43812752+ayslanleal@users.noreply.github.com>
2022-07-11 23:00:17 -0300

    Update postgress_migration_and_join_tables.py

--- dags/postgress_migration_and_join_tables.py
+++ dags/postgress_migration_and_join_tables.py
@@ -4,9 +4,7 @@ from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.hooks.base_hook import BaseHook
 
 default_args = {
